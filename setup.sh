6de8e9cc3f60a0aa8d466b508e274d1409a61768
makaveli10 <suryanvineet47@gmail.com>
2023-05-04 01:44:15 +0800

    bits and bytes fix

--- setup.sh
+++ setup.sh
@@ -6,13 +6,9 @@ apt install wget
 
 pip install -r requirements.txt
 pip install git+https://github.com/huggingface/diffusers
-# git clone https://github.com/makaveli10/diffusion
-# mv diffusion/* .
-# rm -rf diffusion
 
 # dirty hack to make bitsandbytes work on cuda
-# cp /usr/local/lib/python3.8/dist-packages/bitsandbytes/libbitsandbytes_cuda112.so \
-#     /usr/local/lib/python3.8/dist-packages/bitsandbytes/libbitsandbytes_cpu.so
+cp /opt/conda/lib/python3.10/site-packages/bitsandbytes/libbitsandbytes_cuda116.so /opt/conda/lib/python3.10/site-packages/bitsandbytes/libbitsandbytes_cpu.so
 
 # codeformer setup
 git clone https://github.com/sczhou/CodeFormer
