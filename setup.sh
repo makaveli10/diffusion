dd0318ac5e9b41c857d189a95d90c07fa8c92404
makaveli10 <suryanvineet47@gmail.com>
2023-05-04 00:59:27 +0800

    update stup files for vast ai

--- setup.sh
+++ setup.sh
@@ -4,11 +4,17 @@ apt install wget
 
 pip install -r requirements.txt
 pip install git+https://github.com/huggingface/diffusers
-wget -O train_dreambooth.py https://raw.githubusercontent.com/ShivamShrirao/diffusers/main/examples/dreambooth/train_dreambooth.py
-git clone https://github.com/makaveli10/diffusion
-mv diffusion/* .
-rm -rf diffusion
+# git clone https://github.com/makaveli10/diffusion
+# mv diffusion/* .
+# rm -rf diffusion
 
 # dirty hack to make bitsandbytes work on cuda
 # cp /usr/local/lib/python3.8/dist-packages/bitsandbytes/libbitsandbytes_cuda112.so \
 #     /usr/local/lib/python3.8/dist-packages/bitsandbytes/libbitsandbytes_cpu.so
+
+# codeformer setup
+# git clone https://github.com/sczhou/CodeFormer
+# cd CodeFormer
+# pip install -r requirements.txt
+# python basicsr/setup.py develop
+# pip install dlib
