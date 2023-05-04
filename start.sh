5b7e60b41ea8b69463908ceb571d344466edc991
makaveli10 <suryanvineet47@gmail.com>
2023-05-04 20:52:34 +0800

    make bnb setup compatible with vast ai

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # dirty hack to make bitsandbytes work on cuda
 export cuda_ver=cuda"$(python -c 'import torch; print(torch.version.cuda)' | sed -e 's/\.//g')"
 export python_version="$(python --version | sed 's/ //g' | tr '[:upper:]' '[:lower:]')"
-export python_ver=${python_version:0:9}
+export python_ver=${python_version::-2}
 cp /opt/conda/lib/$python_ver/site-packages/bitsandbytes/libbitsandbytes_$cuda_ver.so /opt/conda/lib/$python_ver/site-packages/bitsandbytes/libbitsandbytes_cpu.so
 
 jupyter notebook --allow-root --port=8888 --no-browser --ip=0.0.0.0
